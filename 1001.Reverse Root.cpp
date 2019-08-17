5ea8e81e667d01e7acdbd048f9e67cc0dd90b1ee
Ashraf Tasin <ashraf.tasin.000@gmail.com>
2019-08-17 16:02:22 +0600

    Timus

---
+++ 1001.Reverse Root.cpp
@@ -0,0 +1,55 @@
+/*----------------------|
+/ Author : Ashraf Tasin |
+/ Date   :   08.08.19   |
+/----------------------*/
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define all v.begin(),v.end()
+#define flash ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define db double
+#define endl "\n"
+#define mnm pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>
+#define show for(int i=0;i<v.size();i++) cout << v[i] << " ";
+#define eps 1e-6
+#define mx 100008
+using namespace std;
+
+int dx[]={-1,1,-1,0,1,-1,0,1};
+int dy[]={0,0,-1,-1,-1,1,1,1};
+
+/*Knight Moves
+
+int dx[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int dy[] = { 1, 2, 2, 1, -1, -2, -2, -1 }; */
+
+/*Normal Moves
+
+int x[] = {0, 1, 0, -1};
+int y[] = {-1, 0, 1, 0}; */
+
+
+int main()
+{
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+
+    flash
+
+    vector<double> v;
+    ll x;
+
+    while(cin >> x)
+    {
+        double p=sqrt(x);
+        v.pb(p);
+    }
+
+    int sz=v.size();
+
+    for(int i=sz-1;i>=0;--i) cout << fixed << setprecision(4) << v[i] << endl;
+
+    return 0;
+}
---
+++ 1068.Sum.cpp
@@ -0,0 +1,53 @@
+/*----------------------|
+/ Author : Ashraf Tasin |
+/ Date   :   08.08.19   |
+/----------------------*/
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define all v.begin(),v.end()
+#define flash ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define db double
+#define endl "\n"
+#define mnm pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>
+#define show for(int i=0;i<v.size();i++) cout << v[i] << " ";
+#define eps 1e-6
+#define mx 100008
+using namespace std;
+
+int dx[]={-1,1,-1,0,1,-1,0,1};
+int dy[]={0,0,-1,-1,-1,1,1,1};
+
+/*Knight Moves
+
+int dx[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int dy[] = { 1, 2, 2, 1, -1, -2, -2, -1 }; */
+
+/*Normal Moves
+
+int x[] = {0, 1, 0, -1};
+int y[] = {-1, 0, 1, 0}; */
+
+
+int main()
+{
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+
+    flash
+
+    int n;
+    cin >> n;
+
+    if(n>=1) cout << (n*(n+1))/2 << endl;
+    else if(n==0) cout << 1 << endl;
+    else 
+    {
+        n=abs(n);
+        cout << 0-((n*(n+1))/2)+1 << endl;
+    }
+
+    return 0;
+}
